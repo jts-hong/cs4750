a24c9ff68cb62401a9cfc640502d9e277a027c2a
jts-hong <jhong1711785634@gmail.com>
2022-11-02 23:14:26 -0400

    added table

--- welcome.php
+++ welcome.php
@@ -24,27 +24,56 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <body>
     <?php include('header.html') ?> 
     <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to Ultimate 怨种 Website.</h1>
+
+
+
+    <div class="album py-5 bg-light">
+        <div class="container">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                        <div class="card-body">
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small class="text-muted">9 mins</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <div class="container">
+        <table class="w3-table w3-bordered w3-card-4 center" style="width:70%">
+            <thead>
+                <tr style="background-color:#B0B0B0">
+                    <th width="30%">Make</th>
+                    <th width="30%">Year</th>
+                    <th width="30%">model</th>
+                </tr>
+                </thead>
+                <?php foreach ($list_of_cars as $car_info): ?>
+                <tr>
+                    <td><?php echo $car_info['make']; ?></td>
+                    <td><?php echo $car_info['year']; ?></td>
+                    <td><?php echo $car_info['model']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    </div>
     <p>
         <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
     </p>
-    <div class="container">
-    <table class="w3-table w3-bordered w3-card-4 center" style="width:70%">
-        <thead>
-            <tr style="background-color:#B0B0B0">
-                <th width="30%">Name</th>
-                <th width="30%">Major</th>
-                <th width="30%">Year</th>
-            </tr>
-            </thead>
-            <?php foreach ($list_of_cars as $car_info): ?>
-            <tr>
-                <td><?php echo $car_info['make']; ?></td>
-                <td><?php echo $car_info['year']; ?></td>
-                <td><?php echo $car_info['model']; ?></td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
-    </div>
     <?php include('footer.html') ?>
 </body>
 </html>`
