29b1963e3b0f461259a467717fb1cd4c438ed452
SiyangSun2001 <ss6cuf@virginia.edu>
2022-11-19 14:20:04 -0500

    partial imp of like

--- car-db.php
+++ car-db.php
@@ -2,13 +2,28 @@
 
 function getAllCars()
 {
-    global $link; 
+    global $link;
     $result = mysqli_query($link, "SELECT * FROM vehicle");
     return $result;
 }
- 
 
+function addLike($car_id, $user_id)
+{
+  global $link;
+  $sql = "INSERT INTO likecar VALUES(?, ?)";
+  $stmt = mysqli_prepare($link, $sql);
+  mysqli_stmt_bind_param($stmt, "ii", $user_id, $car_id);
+  mysqli_stmt_execute($stmt);
+  mysqli_stmt_close($stmt);
 
+}
+
+function getAllLikedCars()
+{
+    global $link;
+    $result = mysqli_query($link, "SELECT * FROM vehicle");
+    return $result;
+}
 
 
 
--- welcome.php
+++ welcome.php
@@ -10,7 +10,18 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 ?>
- 
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+  if (!empty($_POST['btnAction']) && $_POST['btnAction'] == 'liked')
+  {
+    echo $_POST['car_to_like'];
+    addLike($_POST['car_to_like'], $_SESSION["user_id"]);
+  }
+}
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,27 +33,27 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     </style>
 </head>
 <body>
-    <?php include('header.html') ?> 
+    <?php include('header.html') ?>
     <br></br>
-    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to CARHUGE</h1>
+    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["user_id"]); ?></b>. Welcome to CARHUGE</h1>
 
-    <?php  
-        $per_page_record = 10;  // Number of entries to show in a page.   
-        if (isset($_GET["page"])) {    
-            $page  = $_GET["page"];    
-        }    
-        else {    
-            $page=1;    
-        }    
-    
-        $start_from = ($page-1) * $per_page_record;     
-    
-        $query = "SELECT * FROM vehicle LIMIT $start_from, $per_page_record";     
-        $rs_result = mysqli_query ($link, $query);    
-    ?>    
-    
-    
-    <?php while ($row = mysqli_fetch_array($rs_result)) { ?>     
+    <?php
+        $per_page_record = 10;  // Number of entries to show in a page.
+        if (isset($_GET["page"])) {
+            $page  = $_GET["page"];
+        }
+        else {
+            $page=1;
+        }
+
+        $start_from = ($page-1) * $per_page_record;
+
+        $query = "SELECT * FROM vehicle LIMIT $start_from, $per_page_record";
+        $rs_result = mysqli_query ($link, $query);
+    ?>
+
+
+    <?php while ($row = mysqli_fetch_array($rs_result)) { ?>
         <div class="album py-5 bg-light">
             <div class="container">
 
@@ -52,61 +63,56 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
                             <div class="card-body">
                                 <p class="card-text">
                                     <?php echo $row['make']; ?>
-                                    <?php echo $row['year']; ?>
+                                    <?php echo $row['car_id']; ?>
                                     <?php echo $row['model']; ?>
                                 </p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                        <button type="button" class="btn btn-sm btn-outline-secondary">Like</button>
-                                    </div>
-                                    <small class="text-muted">9 mins</small>
-                                </div>
+                                <form action="welcome.php" method="post">
+                                  <div class="d-flex justify-content-between align-items-center">
+                                      <div class="btn-group">
+                                          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                          <button type="submit" value = "liked" name="btnAction" class="btn btn-sm btn-outline-secondary">Like</button>
+                                          <input type="hidden" name="car_to_like" value="<?php echo $row['car_id']; ?>"/>
+                                      </div>
+                                      <small class="text-muted">9 mins</small>
+                                  </div>
+                                </form>
+
                             </div>
                         </div>
                     </div>
 
             </div>
         </div>
-    <?php };?>   
-    <?php  
-    $query = "SELECT COUNT(*) FROM vehicle";     
-    $rs_result = mysqli_query($link, $query);     
-    $row = mysqli_fetch_row($rs_result);     
-    $total_records = $row[0];     
-    echo "</br>";     
-    // Number of pages required.   
-    $total_pages = ceil($total_records / $per_page_record);     
-    $pagLink = "";       
-    if($page>=2){   
-        echo "<a class='btn btn-outline-primary' href='welcome.php?page=".($page-1)."'>  Prev </a>";   
-    }         
-    // for ($i=1; $i<=$total_pages; $i++) {   
-    //     if ($i == $page) {   
-    //         $pagLink .= "<a class = 'active' href='welcome.php?page="  
-    //                                             .$i."'>".$i." </a>";   
-    //     }               
-    //     else  {   
-    //         $pagLink .= "<a href='welcome.php?page=".$i."'>   
-    //                                             ".$i." </a>";     
-    //     }   
-    // };     
-    echo $pagLink;   
-    if($page<$total_pages){   
-        echo "<a class='btn btn-outline-primary' href='welcome.php?page=".($page+1)."'>  Next </a>";  
+    <?php };?>
+    <?php
+    $query = "SELECT COUNT(*) FROM vehicle";
+    $rs_result = mysqli_query($link, $query);
+    $row = mysqli_fetch_row($rs_result);
+    $total_records = $row[0];
+    echo "</br>";
+    // Number of pages required.
+    $total_pages = ceil($total_records / $per_page_record);
+    $pagLink = "";
+    if($page>=2){
+        echo "<a class='btn btn-outline-primary' href='welcome.php?page=".($page-1)."'>  Prev </a>";
     }
-    ?>      
-        <input id="page" type="number" min="1" max="<?php echo $total_pages?>"   
-        placeholder="<?php echo $page."/".$total_pages; ?>" required>   
-        <button onClick="go2Page();"class="btn btn-primary">Go</button>   
-    <script>   
-        function go2Page()   
-        {   
-            var page = document.getElementById("page").value;   
-            page = ((page><?php echo $total_pages; ?>)?<?php echo $total_pages; ?>:((page<1)?1:page));   
-            window.location.href = 'welcome.php?page='+page;   
-        }   
-    </script>  
+
+    echo $pagLink;
+    if($page<$total_pages){
+        echo "<a class='btn btn-outline-primary' href='welcome.php?page=".($page+1)."'>  Next </a>";
+    }
+    ?>
+        <input id="page" type="number" min="1" max="<?php echo $total_pages?>"
+        placeholder="<?php echo $page."/".$total_pages; ?>" required>
+        <button onClick="go2Page();"class="btn btn-primary">Go</button>
+    <script>
+        function go2Page()
+        {
+            var page = document.getElementById("page").value;
+            page = ((page><?php echo $total_pages; ?>)?<?php echo $total_pages; ?>:((page<1)?1:page));
+            window.location.href = 'welcome.php?page='+page;
+        }
+    </script>
     <br></br>
     <br></br>
     <?php include('footer.html') ?>
