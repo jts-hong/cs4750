62330354a42cc76f82a8658f2adde309aa6aedae
jts-hong <jhong1711785634@gmail.com>
2022-11-21 17:13:47 -0500

    minor

--- searchByYear.php
+++ searchByYear.php
@@ -1,17 +1,31 @@
 <?php
 // Initialize the session
 session_start();
+require("car-db.php");
 require("config.php");
-
+$list_of_cars = getAllCars();
+$list_of_liked_Cars = getAllLikedCars($_SESSION["user_id"]);
 // Check if the user is logged in, if not then redirect him to login page
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: login.php");
     exit;
 }
-require("car-db.php");
 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (!empty($_POST['btnAction']) && $_POST['btnAction'] == 'liked') {
+        echo $_POST['car_to_like'];
+        addLike($_POST['car_to_like'], $_SESSION["user_id"]);
+        $list_of_liked_Cars = getAllLikedCars($_SESSION["user_id"]);
+    } elseif (isset($_POST['car_id']) && $_POST['to_like'] == 1) {
+        addLike($_POST["car_id"], $_SESSION["user_id"]);
+        die();
+    } elseif (isset($_POST['car_id']) && $_POST['to_like'] == 0) {
+        removeLIke($_POST["car_id"], $_SESSION["user_id"]);
+        die();
+    }
+}
 $raw_results = Null;
-
 ?>
 
 <!DOCTYPE html>
@@ -46,65 +60,113 @@ $raw_results = Null;
     </br>
     <?php
 
-        if ($_SERVER["REQUEST_METHOD"] == "POST"){
-            $query = intval($_POST['year']);
-            $min_length = 0;
-            if (strlen($query) >= $min_length) { // if query length is more or equal minimum length then
-        
-                $query = htmlspecialchars($query);
-                $q = "SELECT * FROM vehicle WHERE (year= '$query') ;";
-                $raw_results = mysqli_query($link, $q);
-                // * means that it selects all fields, you can also write: `id`, `title`, `text`
-                // articles is the name of our table
-        
-                // '%$query%' is what we're looking for, % means anything, for example if $query is Hello
-                // it will match "hello", "Hello man", "gogohello", if you want exact match use `title`='$query'
-                // or if you want to match just full word so "gogohello" is out use '% $query %' ...OR ... '$query %' ... OR ... '% $query'
-        
-                if (mysqli_num_rows($raw_results) > 0) { // if one or more rows are returned do following
-                    while ($row = mysqli_fetch_array($raw_results)) { ?>
-                        <div class="album py-5 bg-light">
-                            <div class="container">
-                                <div class="col">
-                                    
-                                    <div class="card shadow-sm">
-                                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                                            <title>Placeholder</title>
-                                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                                        </svg>
-                                        <div class="card-body">
-                                            <h2 class="card-text">
-                                                <?php echo $row['make']; ?>
-                                                <?php echo $row['year']; ?>
-                                                <?php echo $row['model']; ?>
-                                            </h2>
-                                            <div class="d-flex justify-content-between align-items-center">
-                                                <div class="btn-group">
-                                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                                    <button type="button" class="btn btn-sm btn-outline-secondary">Like</button>
-                                                </div>
-                                                <small class="text-muted">9 mins</small>
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $query = $_POST['year'];
+        $min_length = 0;
+        if (strlen($query) >= $min_length) { // if query length is more or equal minimum length then
+
+            $query = htmlspecialchars($query);
+            $q = "SELECT * FROM vehicle WHERE (year= '$query') ;";
+            $raw_results = mysqli_query($link, $q);
+            // * means that it selects all fields, you can also write: `id`, `title`, `text`
+            // articles is the name of our table
+
+            // '%$query%' is what we're looking for, % means anything, for example if $query is Hello
+            // it will match "hello", "Hello man", "gogohello", if you want exact match use `title`='$query'
+            // or if you want to match just full word so "gogohello" is out use '% $query %' ...OR ... '$query %' ... OR ... '% $query'
+
+            if (mysqli_num_rows($raw_results) > 0) { // if one or more rows are returned do following
+                while ($row = mysqli_fetch_array($raw_results)) { ?>
+                    <div class="album py-5 bg-light">
+                        <div class="container">
+                            <div class="col">
+
+                                <div class="card shadow-sm">
+                                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                                        <title>Placeholder</title>
+                                        <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                                    </svg>
+                                    <div class="card-body">
+                                        <h2 class="card-text">
+                                            <?php echo $row['make']; ?>
+                                            <?php echo $row['year']; ?>
+                                            <?php echo $row['model']; ?>
+                                        </h2>
+
+                                        <?php
+                                        $like_btn_color = "#FFFFFF";
+                                        $like_btn_class = "";
+                                        foreach ($list_of_liked_Cars as $curCar) :
+                                            if ($curCar['car_id'] == $row['car_id']) {
+                                                $like_btn_color = "red";
+                                                $like_btn_class = "liked";
+                                            }
+                                        endforeach;
+                                        ?>
+
+                                        <!-- like button on each car post -->
+                                        <button id=<?php echo $row['car_id']; ?> class=<?php echo $like_btn_class; ?> style="background-color:<?php echo $like_btn_color; ?>;">Like</button>
+
+                                        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+
+                                        <script>
+                                            // get the button using id which is the car_id
+                                            $("#<?php echo $row['car_id']; ?>").click(function(e) {
+                                                if ($(this).hasClass("liked")) {
+                                                    $(this).removeClass("liked");
+                                                    document.getElementById(<?php echo $row['car_id']; ?>).style.background = "#FFFFFF";
+                                                    $.post("welcome.php", {
+                                                        "to_like": 0,
+                                                        "car_id": <?php echo $row['car_id']; ?>
+                                                    });
+                                                } else {
+                                                    $(this).addClass("liked");
+                                                    // change appearance of button 
+                                                    document.getElementById(<?php echo $row['car_id']; ?>).style.background = "red";
+                                                    // send post to php to modify database    
+                                                    $.post("welcome.php", {
+                                                        "to_like": 1,
+                                                        "car_id": <?php echo $row['car_id']; ?>
+                                                    });
+                                                }
+                                            });
+                                        </script>
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div class="btn-group">
+
+
+                                                <!-- <button type="button" class="btn btn-sm btn-outline-secondary">View</button> -->
+                                                <!-- <button type="submit" value = "liked" name="btnAction" class="btn btn-sm btn-outline-secondary">Like</button> -->
+                                                <a href="view.php?car_id=<?php echo $row['car_id']; ?>"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
+                                                <input type="hidden" name="car_to_like" value="<?php echo $row['car_id']; ?>" />
+
                                             </div>
+                                            <?php foreach ($list_of_liked_Cars as $curCar) :
+                                                if ($curCar['car_id'] == $row['car_id']) {
+                                                    echo "liked this car";
+                                                }
+                                            endforeach; ?>
                                         </div>
                                     </div>
                                 </div>
-                
                             </div>
+
                         </div>
-                    <?php };
-                } else { // if there is no matching rows do following
-                    echo "No results";
-                }
-            } else { // if query length is less than minimum
-                echo "Minimum length is " . $min_length;
+                    </div>
+    <?php };
+            } else { // if there is no matching rows do following
+                echo "No results";
             }
+        } else { // if query length is less than minimum
+            echo "Minimum length is " . $min_length;
         }
-        else{
-            $raw_results =getAllCars();
-        }
-    
+    } else {
+        $raw_results = getAllCars();
+    }
+
 
     ?>
+
 
     </br>
     </br>
--- welcome.php
+++ welcome.php
@@ -14,22 +14,18 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 ?>
 
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-  if (!empty($_POST['btnAction']) && $_POST['btnAction'] == 'liked')
-  {
-    echo $_POST['car_to_like'];
-    addLike($_POST['car_to_like'], $_SESSION["user_id"]);
-    $list_of_liked_Cars = getAllLikedCars($_SESSION["user_id"]);
-  }elseif(isset($_POST['car_id']) && $_POST['to_like'] ==1 )
-  {
-      addLike($_POST["car_id"], $_SESSION["user_id"]);
-      die();
-      
-  }elseif(isset($_POST['car_id']) && $_POST['to_like'] ==0){
-      removeLIke($_POST["car_id"], $_SESSION["user_id"]);
-      die();
-  }
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (!empty($_POST['btnAction']) && $_POST['btnAction'] == 'liked') {
+        echo $_POST['car_to_like'];
+        addLike($_POST['car_to_like'], $_SESSION["user_id"]);
+        $list_of_liked_Cars = getAllLikedCars($_SESSION["user_id"]);
+    } elseif (isset($_POST['car_id']) && $_POST['to_like'] == 1) {
+        addLike($_POST["car_id"], $_SESSION["user_id"]);
+        die();
+    } elseif (isset($_POST['car_id']) && $_POST['to_like'] == 0) {
+        removeLIke($_POST["car_id"], $_SESSION["user_id"]);
+        die();
+    }
 }
 ?>
 
@@ -52,9 +48,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     <?php include('header.html') ?>
     <br></br>
     <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["user_id"]); ?></b>. Welcome to CARHUGE</h1>
-    <li>
-        <a href="addcar.php" class="btn btn-danger ml-3"> Add Car</a>
-    </li>
+    <a href="addcar.php" class="btn btn-danger ml-3"> Add Car</a>
+    <br></br>
+    <br></br>
     <?php
     $per_page_record = 10;  // Number of entries to show in a page.   
     if (isset($_GET["page"])) {
@@ -75,46 +71,55 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
         <div class="album py-5 bg-light">
             <div class="container">
 
-                    <div class="col">
-                        <div class="card shadow-sm">
-                            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                            <div class="card-body">
-                                <p class="card-text">
-                                    <?php echo $row['make']; ?>
-                                    <?php echo $row['year']; ?>
-                                    <?php echo $row['model']; ?>
-                                </p>
-                                
-                                <!-- for loop to get the status of like button, if liked assign class and change color -->
-                                <?php 
-                                $like_btn_color = "#FFFFFF";
-                                $like_btn_class = "";
-                                foreach ($list_of_liked_Cars as $curCar):
-                                            if ($curCar['car_id'] == $row['car_id']){
-                                                $like_btn_color = "red";
-                                                $like_btn_class = "liked";
-                                            }
-                                        endforeach;
-                                ?>
-                                                            
-                                <!-- like button on each car post -->
-                                <button id=<?php echo $row['car_id']; ?> class = <?php echo $like_btn_class; ?> style = "background-color:<?php echo $like_btn_color; ?>;">Like</button>
-                                
-                                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
-                                
-                                <script> 
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <title>Placeholder</title>
+                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                        </svg>
+                        <div class="card-body">
+                            <h2 class="card-text">
+                                <?php echo $row['make']; ?>
+                                <?php echo $row['year']; ?>
+                                <?php echo $row['model']; ?>
+                            </h2>
+
+                            <!-- for loop to get the status of like button, if liked assign class and change color -->
+                            <?php
+                            $like_btn_color = "#FFFFFF";
+                            $like_btn_class = "";
+                            foreach ($list_of_liked_Cars as $curCar) :
+                                if ($curCar['car_id'] == $row['car_id']) {
+                                    $like_btn_color = "red";
+                                    $like_btn_class = "liked";
+                                }
+                            endforeach;
+                            ?>
+
+                            <!-- like button on each car post -->
+                            <button id=<?php echo $row['car_id']; ?> class=<?php echo $like_btn_class; ?> style="background-color:<?php echo $like_btn_color; ?>;">Like</button>
+
+                            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+
+                            <script>
                                 // get the button using id which is the car_id
-                                $("#<?php echo $row['car_id']; ?>").click(function(e){
-                                    if($(this).hasClass("liked")){                 
-                                        $(this).removeClass("liked");                                                                                
+                                $("#<?php echo $row['car_id']; ?>").click(function(e) {
+                                    if ($(this).hasClass("liked")) {
+                                        $(this).removeClass("liked");
                                         document.getElementById(<?php echo $row['car_id']; ?>).style.background = "#FFFFFF";
-                                        $.post("welcome.php",{"to_like": 0 ,"car_id":<?php echo $row['car_id']; ?>});
+                                        $.post("welcome.php", {
+                                            "to_like": 0,
+                                            "car_id": <?php echo $row['car_id']; ?>
+                                        });
                                     } else {
                                         $(this).addClass("liked");
                                         // change appearance of button 
                                         document.getElementById(<?php echo $row['car_id']; ?>).style.background = "red";
                                         // send post to php to modify database    
-                                        $.post("welcome.php",{"to_like": 1, "car_id":<?php echo $row['car_id']; ?>});
+                                        $.post("welcome.php", {
+                                            "to_like": 1,
+                                            "car_id": <?php echo $row['car_id']; ?>
+                                        });
                                     }
                                 });
                                 </script>
@@ -127,22 +132,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
                                           <!-- <button type="submit" value = "liked" name="btnAction" class="btn btn-sm btn-outline-secondary">Like</button> -->
                                           <a href="view.php?car_id=<?php echo $row['car_id'];?>"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
                                           <input type="hidden" name="car_to_like" value="<?php echo $row['car_id']; ?>"/>
-                                          <?php foreach ($list_of_liked_Cars as $curCar):
+                                          
+                                    </div>
+                                    <?php foreach ($list_of_liked_Cars as $curCar):
                                             if ($curCar['car_id'] == $row['car_id']){
                                                 echo "liked this car";
                                             }
-                                        endforeach;?>
-                                      </div>
-                                      <small class="text-muted">9 mins</small>
-                                  </div>
-                                </form>
+                                        endforeach; ?>
+                                </div>
+                            </form>
 
-                            </div>
                         </div>
                     </div>
                 </div>
-
             </div>
+
+        </div>
         </div>
     <?php }; ?>
     <?php
@@ -176,4 +181,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     <br></br>
     <?php include('footer.html') ?>
 </body>
+
 </html>
